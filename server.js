618cf93ee2ace397d758c6fd53cd839c57591821
samvel <hedgehog.millenium@gmail.com>
2024-09-25 16:35:57 +0400

    clear git cache

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express')
 const app = express()
 var bodyParser = require('body-parser');
-const path = require('path');
 
 const index = require('./routes/index')
 const registry = require('./routes/registry')
 
-app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 // app.set('views', path.join(__dirname, 'views'));
 
-app.use('/',index)
-app.use('/registry',registry)
+app.use('/', index)
+app.use('/registry', registry)
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('Listening on port 3000')
 })
 
